a92a407fed950bd1e3666ffd08a442a34a6c9b56
FlorianHegele <144370530+FlorianHegele@users.noreply.github.com>
2025-01-27 16:41:33 +0100

    ajout des composants 7 et 8

---
+++ src/views/BankAmount.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <slot name="account-amount" :amount="amountAccount">
+      {{ amountAccount }}
+    </slot>
+  </div>
+</template>
+
+<script>
+
+import {mapState} from "vuex";
+
+export default {
+  name: 'BankAmount',
+  computed: {
+    ...mapState('bank', ['accountAmount']),
+    amountAccount() {
+      return this.accountAmount;
+    }
+  },
+}
+</script>
---
+++ src/views/BankHome.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Bienvenue à la Banque</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BankHome',
+}
+</script>
