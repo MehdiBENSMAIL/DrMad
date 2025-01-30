aaacc39e8425ac652d6e55e410e1622b280110a2
MehdiBENSMAIL <mehdi.bensmail@protonmail.com>
2025-01-30 10:11:12 +0100

    formatting

--- src/components/VerticalMenu.vue
+++ src/components/VerticalMenu.vue
@@ -1,48 +1,47 @@
 <template>
-  <div class="vertical-menu">
-    <div v-for="(item, index) in items" :key="index" class="menu-item">
-      <template v-if="item.type === 'title'">
-        <slot name="menu-title" :label="item.label">
-          {{ item.label }}
-        </slot>
-      </template>
+	<div class="vertical-menu">
+		<div v-for="(item, index) in items" :key="index" class="menu-item">
+			<template v-if="item.type === 'title'">
+				<slot name="menu-title" :label="item.label">
+					{{ item.label }}
+				</slot>
+			</template>
 
-      <template v-if="item.type === 'link'">
-        <span @click="goTo(item.to)" class="menu-link">
-          <slot name="menu-link" :label="item.label">
-            <button>{{ item.label }}</button>
-          </slot>
-        </span>
-      </template>
-    </div>
-  </div>
+			<template v-if="item.type === 'link'">
+				<span @click="goTo(item.to)" class="menu-link">
+					<slot name="menu-link" :label="item.label">
+						<button>{{ item.label }}</button>
+					</slot>
+				</span>
+			</template>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  name: "VerticalMenu",
-  props: {items: Array},
-  methods: {
-    goTo(dest) {
-      this.$router.push(dest);
-    }
-  }
+	name: "VerticalMenu",
+	props: { items: Array },
+	methods: {
+		goTo(dest) {
+			this.$router.push(dest);
+		}
+	}
 }
 </script>
 
 <style scoped>
 .vertical-menu {
-  display: flex;
-  flex-direction: column;
+	display: flex;
+	flex-direction: column;
 }
 
 .menu-item {
-  margin: 5px 0;
+	margin: 5px 0;
 }
 
 .menu-link {
-  cursor: pointer;
-  padding-left: 10px;
+	cursor: pointer;
+	padding-left: 10px;
 }
-
 </style>
